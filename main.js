375b2f33fe24248908b547e8fcc433ab48da2635
santipu_ <santipujolsau@gmail.com>
2022-04-07 13:55:31 +0200

    Set initial turn to random inside gameFlow module

--- main.js
+++ main.js
@@ -168,7 +168,9 @@ const modal = (() => {
 const gameFlow = (() => {
 
     // set xTurn to random
-    let xTurn = true;
+    const _setTurn = () => Math.floor(Math.random()*2);
+    let xTurn = (_setTurn()) ? true : false;
+    
     const winCombos = [
         [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
     ]
@@ -261,7 +263,7 @@ const gameFlow = (() => {
         gameBoard.clearGrid();
         gameBoard.renderGrid();
         gameBoard.clearPlayerMoves();
-        xTurn = true; 
+        xTurn = (_setTurn()) ? true : false; 
         indicateTurn();
     }
 
